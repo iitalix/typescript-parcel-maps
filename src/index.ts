44859e24a7d39f424d0d042531e0c07d50f393e3
Lan Oribello <lan@pompbeauty.com>
2024-08-26 15:33:46 -0700

    Added Google Maps to index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
 
-const user = new User();
-const company = new Company();
+/* this file is used to import all necessary ts files,
+which will be used in the index.html file */
 
-console.log(user);
-console.log(company);
+// import { User } from './User';
+// import { Company } from './Company';
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
