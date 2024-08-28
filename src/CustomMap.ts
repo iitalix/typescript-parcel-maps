210a0e495cced1d9fce1400a9a196c7ba67f4def
Lan Oribello <lan@pompbeauty.com>
2024-08-28 12:17:06 -0700

    Added markerContent components to CustomMap, User, and Company.

---
+++ src/CustomMap.ts
@@ -0,0 +1,61 @@
+/// <reference types="@types/google.maps" />
+import { User } from './User';
+import { Company } from './Company';
+
+// Instructions to every other class on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  // make private so that no one can access instance
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+
+  // addMarker(mappable: User | Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: mappable.location.lat,
+  //       lng: mappable.location.lng,
+  //     },
+  //   });
+  // }
+
+  /* this solution is not scalable due to dependencies between User vs Company classes;
+  not best solution; this is why we use interfaces */
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 /// <reference types="@types/google.maps" />
-
 /* this file is used to import all necessary ts files,
 which will be used in the index.html file */
 
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
