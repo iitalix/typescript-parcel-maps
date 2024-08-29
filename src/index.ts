8087897895647267be31744139c276c815c7b834
Lan Oribello <lan@pompbeauty.com>
2024-08-29 10:19:43 -0700

    Added notes and Sorter project code to new folder.

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@
 /* this file is used to import all necessary ts files,
 which will be used in the index.html file */
 
+// import Classes
 import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
+// generate new instances of each Class
 const user = new User();
 const company = new Company();
 const customMap = new CustomMap('map');
 
+// call methods to use in index.html file
 customMap.addMarker(user);
 customMap.addMarker(company);
